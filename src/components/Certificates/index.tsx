db1a706655e780d9b75e3766a7bf2b5d96d1aabf
Osaid Alhomedy <o.alhomedy@gmail.com>
2023-03-16 15:23:08 +0300

    refactor some code

--- src/components/Certificates/index.tsx
+++ src/components/Certificates/index.tsx
@@ -1,6 +1,40 @@
+import type { JSXChildren } from "@builder.io/qwik";
 import { component$ } from "@builder.io/qwik";
 
+type Certificate = {
+  name: JSXChildren;
+  comapny: JSXChildren;
+  logo: string;
+  alt: string;
+  badge: {
+    href: string;
+    src: string;
+    alt: string;
+  };
+  description: JSXChildren[];
+};
+
 export const Certificates = component$(() => {
+  const certificates: Certificate[] = [
+    {
+      name: "AWS Certified Cloud Practitioner",
+      comapny: "Amazon",
+      logo: "/icons/AWSIcon.svg",
+      alt: "AWS Logo",
+      badge: {
+        href: "https://www.credly.com/badges/6db6a0d6-2e3f-4614-a3f5-cbb5a09aca22/public_url",
+        src: "/images/CCPBadge.png",
+        alt: "AWS Badge",
+      },
+      description: [
+        "Fundamental concepts of AWS cloud computing, including the benefits, different deployment models, and the global infrastructure.",
+        "Recognize the AWS shared responsibility model, security and compliance processes, and access management.",
+        "AWS core services and their features, as well as the basic architecture of AWS.",
+        "AWS billing and pricing models, account structures, and cost optimization practices.",
+        "Understand the different support plans available to AWS customers, as well as the various documentation and training resources available."
+      ],
+    },
+  ];
   return (
     <div class="md:scroll-m-16">
       <div>
@@ -10,47 +44,46 @@ export const Certificates = component$(() => {
           </span>
         </h2>
         <ul class="flex flex-wrap gap-6">
-          <li class="flex justify-start w-full gap-6 rounded-md border border-details bg-misc p-6 shadow-sm transition-all">
-            <div class="flex flex-col justify-between gap-3">
-              <div class="flex items-center gap-2">
-                <img src="/icons/AWSIcon.svg" alt="AWS Logo" class="h-7 w-7" />
-                <div class="flex items-baseline gap-2">
-                  <p class="flex gap-4 align-baseline font-squada text-sm">
-                    Amazon Web Services
-                  </p>
-                </div>
-              </div>
-              <ul class="flex flex-col gap-1 pl-1">
-                <li class="list-disc-custom flex text-sm">
-                  <h3>
-                    Frontend maintenance for web applications related to the
-                    telecommunications sector
-                  </h3>
-                </li>
-                <li class="list-disc-custom flex text-sm">
-                  <h3>
-                    Frontend maintenance for web applications related to the
-                    tourism and hotel sector
-                  </h3>
-                </li>
-              </ul>
-            </div>
-            <div class="flex items-center justify-center gap-2 w-full sm:w-2/3 md:w-1/2">
-              <span class="rounded-lg bg-misc p-1 transition-all hover:scale-110">
-                <a
-                  href="https://www.credly.com/badges/6db6a0d6-2e3f-4614-a3f5-cbb5a09aca22/public_url"
-                  target="_blank"
-                >
+          {certificates.map((certificate) => (
+            <li class="flex flex-col md:flex-row justify-start w-full gap-6 rounded-md border border-details bg-misc p-6 shadow-sm transition-all">
+              <div class="flex flex-col justify-between gap-3">
+                <div class="flex items-center gap-2">
                   <img
-                    src="/images/CCPBadge.png"
-                    width="100"
-                    height="100"
-                    alt="java logo"
+                    src={certificate.logo}
+                    alt={certificate.alt}
+                    class="h-7 w-7"
                   />
-                </a>
-              </span>
-            </div>
-          </li>
+                  <div class="flex items-baseline gap-2">
+                    <p class="flex gap-4 align-baseline font-squada text-sm">
+                      {certificate.name}
+                    </p>
+                  </div>
+                </div>
+                <ul class="flex flex-col gap-1 pl-1">
+                  {certificate.description.map((description) => (
+                    <li class="list-disc-custom flex text-sm">
+                      <h3>{description}</h3>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div class="flex items-center justify-center gap-2 w-full sm:w-2/3 md:w-1/2">
+                <span class="rounded-lg bg-misc p-1 transition-all hover:scale-110">
+                  <a
+                    href={certificate.badge.href}
+                    target="_blank"
+                  >
+                    <img
+                      src={certificate.badge.src}
+                      width="100"
+                      height="100"
+                      alt="java logo"
+                    />
+                  </a>
+                </span>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/components/Experiance/index.tsx
+++ src/components/Experiance/index.tsx
@@ -1,14 +1,154 @@
-import { component$, JSXChildren, useSignal } from "@builder.io/qwik";
+import type { JSXChildren } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
+
+type Experience = {
+  companyName: JSXChildren;
+  companyLogo: JSXChildren;
+  wideCompanyLogo: Boolean;
+  position: JSXChildren;
+  positionTime: JSXChildren;
+  positionDescription: JSXChildren[];
+  positionSkills: {
+    skillAlt: JSXChildren;
+    skillLogo: JSXChildren;
+  }[];
+};
 
 export const Experience = component$(() => {
-  const getExperience = (): JSXChildren => {
-    const diff: number =
-      new Date().getTime() - new Date("2022-07-01").getTime();
-    const diffMonths: number = Math.ceil(diff / (1000 * 60 * 60 * 24 * 30));
-    return diffMonths.toString();
-  };
 
-  const experience = useSignal(getExperience());
+  const calculateDateFromToday = (date: Date) : JSXChildren => {
+    const today = new Date();
+    const diff = Math.abs(today.getTime() - date.getTime());
+    const diffInMonths = Math.ceil(diff / (1000 * 3600 * 24 * 30));
+    return diffInMonths;
+  }
+
+  const experiances: Experience[] = [
+    {
+      companyName: "Intrasoft MiddleEast",
+      wideCompanyLogo: false,
+      companyLogo: "/images/IntrasoftLogo.jpg",
+      position: "Full Stack Java Developer",
+      positionTime: `${calculateDateFromToday(new Date("2022-07-01"))} Months`,
+      positionDescription: [
+        "Develop and maintain scalable and maintainable backend applications in cloud based environment",
+        "Write clean, maintainable and scalable code",
+        "Collaborate with cross-functional teams to deliver high quality solutions that meet business objectives",
+        "Participate in code reviews, design discussions and architecture reviews",
+        "Work in an Agile software development environment and follow Agile methodologies"
+      ],
+      positionSkills: [
+        {
+          skillAlt: "java logo",
+          skillLogo: "/icons/JavaIcon.svg",
+        },
+        {
+          skillAlt: "spring logo",
+          skillLogo: "/icons/SpringIcon.svg",
+        },
+        {
+          skillAlt: "postgresql logo",
+          skillLogo: "/icons/PostgresIcon.svg",
+        },
+        {
+          skillAlt: "maven logo",
+          skillLogo: "/icons/MavenIcon.svg",
+        },
+        {
+          skillAlt: "javascript logo",
+          skillLogo: "/icons/JSIcon.svg",
+        },
+        {
+          skillAlt: "react logo",
+          skillLogo: "/icons/ReactIcon.svg",
+        },
+        {
+          skillAlt: "redux logo",
+          skillLogo: "/icons/ReduxIcon.svg",
+        },
+        {
+          skillAlt: "tailwind logo",
+          skillLogo: "/icons/TailwindIcon.svg",
+        },
+        {
+          skillAlt: "postcss logo",
+          skillLogo: "/icons/PostcssIcon.svg",
+        },
+        {
+          skillAlt: "html logo",
+          skillLogo: "/icons/HTMLIcon.svg",
+        },
+        {
+          skillAlt: "css logo",
+          skillLogo: "/icons/CSSIcon.svg",
+        },
+        {
+          skillAlt: "git logo",
+          skillLogo: "/icons/GitIcon.svg",
+        },
+        {
+          skillAlt: "github logo",
+          skillLogo: "/icons/GithubIcon.svg",
+        },
+        {
+          skillAlt: "linux logo",
+          skillLogo: "/icons/LinuxIcon.svg",
+        },
+        {
+          skillAlt: "docker logo",
+          skillLogo: "/icons/DockerIcon.svg",
+        },
+        {
+          skillAlt: "sap logo",
+          skillLogo: "/icons/SAPIcon.svg",
+        },
+      ],
+    },
+    {
+      companyName: "Luminus Technical University College LTUC",
+      companyLogo: "/images/LTUCLogo.png",
+      wideCompanyLogo: true,
+      position: "Software Development Teaching Assistant",
+      positionTime: "7 Months",
+      positionDescription: [
+        "Assist the instructor in the development of course materials, such as lecture slides, assignments, and assessments",
+        "Conduct review sessions and tutorials for students",
+        "Assist in the delivery of lectures, including leading discussions, demonstrations, and answering questions",
+        "Provide support and guidance to students as they complete assignments and projects",
+        "Grade assignments and provide constructive feedback to students",
+      ],
+      positionSkills: [
+        {
+          skillAlt: "java logo",
+          skillLogo: "/icons/JavaIcon.svg",
+        },
+        {
+          skillAlt: "spring logo",
+          skillLogo: "/icons/SpringIcon.svg",
+        },
+        {
+          skillAlt: "javascript logo",
+          skillLogo: "/icons/JSIcon.svg",
+        },
+        {
+          skillAlt: "react logo",
+          skillLogo: "/icons/ReactIcon.svg",
+        },
+        {
+          skillAlt: "git logo",
+          skillLogo: "/icons/GitIcon.svg",
+        },
+        {
+          skillAlt: "github logo",
+          skillLogo: "/icons/GithubIcon.svg",
+        },
+        {
+          skillAlt: "linux logo",
+          skillLogo: "/icons/LinuxIcon.svg",
+        },
+      ],
+    },
+  ];
 
   return (
     <div class="md:scroll-m-16">
@@ -19,253 +159,51 @@ export const Experience = component$(() => {
           </span>
         </h2>
         <ul class="flex flex-wrap gap-6">
-          <li class="flex w-full gap-12 rounded-md border border-details bg-misc p-6 shadow-sm transition-all">
-            <div class="flex flex-col justify-between gap-3">
-              <div class="flex items-center gap-2">
-                <img
-                  src="/images/IntrasoftLogo.jpg"
-                  alt="RkPeople Logo"
-                  class="h-7 w-7"
-                />
-                <div class="flex items-baseline gap-2">
-                  <p class="flex gap-4 align-baseline font-squada text-sm">
-                    Intrasoft MiddleEast
-                  </p>
-                  <span class="rounded-full border border-details px-2 py-1 text-xs">
-                    {experience.value} months
-                  </span>
+          {experiances.map((experience) => (
+            <li class="flex w-full gap-12 rounded-md border border-details bg-misc p-6 shadow-sm transition-all">
+              <div class="flex flex-col justify-between gap-3">
+                <div class="flex items-center gap-2">
+                  <img
+                    src={experience.companyLogo?.toString()}
+                    alt={experience.companyName?.toString()}
+                    class={experience.wideCompanyLogo ? "h-7 w-10" : "h-7 w-7"}
+                  />
+                  <div class="flex items-baseline gap-2">
+                    <p class="flex align-baseline text-sm">
+                      {experience.companyName}
+                    </p>
+                    <span class="rounded-full border border-details px-2 py-1 text-xs">
+                      {experience.positionTime}
+                    </span>
+                  </div>
+                </div>
+                <ul class="flex flex-col gap-1 pl-1">
+                  <li class="flex text-sm">
+                    <h3 class="font-semibold">{experience.position}</h3>
+                  </li>
+                  <li class="flex text-sm">
+                    <ul>
+                      {experience.positionDescription.map((desc) => (
+                        <li class="list-disc-custom flex">{desc}</li>
+                      ))}
+                    </ul>
+                  </li>
+                </ul>
+                <div class="flex flex-wrap gap-2">
+                  {experience.positionSkills.map((skill) => (
+                    <span class="rounded-lg bg-misc p-1 transition-all">
+                      <img
+                        src={skill.skillLogo?.toString()}
+                        width="24"
+                        height="24"
+                        alt={skill.skillAlt?.toString()}
+                      />
+                    </span>
+                  ))}
                 </div>
               </div>
-              <ul class="flex flex-col gap-1 pl-1">
-                <li class="flex text-sm">
-                  <h3 class="font-semibold">Java Software Developer</h3>
-                </li>
-                <li class="flex text-sm">
-                  <ul>
-                    <li class="list-disc-custom flex">
-                      Assist the instructor in the development of course
-                      materials, such as lecture slides, assignments, and
-                      assessments
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <div class="flex flex-wrap gap-2">
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/JavaIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="java logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/SpringIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="spring logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/PostgresIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="postgresql logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/MavenIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="maven logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/JSIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="javascript logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/ReactIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="react logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/ReduxIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="redux logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/TailwindIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="tailwind logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/PostcssIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="postcss logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/GitIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="git logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/LinuxIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="linux logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/DockerIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="docker logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/AWSIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="sap logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/SAPIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="sap logo"
-                  />
-                </span>
-              </div>
-            </div>
-          </li>
-          <li class="flex w-full gap-12 rounded-md border border-details bg-misc p-6 shadow-sm transition-all">
-            <div class="flex flex-col justify-between gap-3">
-              <div class="flex items-center gap-2">
-                <img
-                  src="/images/LTUCLogo.png"
-                  alt="RkPeople Logo"
-                  class="h-7 w-10"
-                />
-                <div class="flex items-baseline gap-2">
-                  <p class="flex gap-4 align-baseline font-squada text-sm">
-                    Luminus Technical University College LTUC
-                  </p>
-                  <span class="rounded-full border border-details px-2 py-1 text-xs">
-                    7 months
-                  </span>
-                </div>
-              </div>
-              <ul class="flex flex-col gap-1 pl-1">
-                <li class="flex text-sm">
-                  <h3 class="font-semibold">
-                    Software Development Teaching Assistant
-                  </h3>
-                </li>
-                <li class="flex text-sm">
-                  <ul>
-                    <li class="list-disc-custom flex">
-                      Assist the instructor in the development of course
-                      materials, such as lecture slides, assignments, and
-                      assessments
-                    </li>
-                    <li class="list-disc-custom flex">
-                      Conduct review sessions and tutorials for students
-                    </li>
-                    <li class="list-disc-custom flex">
-                      Assist in the delivery of lectures, including leading
-                      discussions, demonstrations, and answering questions
-                    </li>
-                    <li class="list-disc-custom flex">
-                      Provide support and guidance to students as they complete
-                      assignments and projects
-                    </li>
-                    <li class="list-disc-custom flex">
-                      Grade assignments and provide constructive feedback to
-                      students
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <div class="flex flex-wrap gap-2">
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/JavaIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="java logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/SpringIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="spring logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/JSIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="javascript logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/ReactIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="react logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/GitIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="git logo"
-                  />
-                </span>
-                <span class="rounded-lg bg-misc p-1 transition-all">
-                  <img
-                    src="/icons/LinuxIcon.svg"
-                    width="24"
-                    height="24"
-                    alt="linux logo"
-                  />
-                </span>
-              </div>
-            </div>
-          </li>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
