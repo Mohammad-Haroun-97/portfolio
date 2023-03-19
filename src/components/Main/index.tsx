a4a7433685adc7e8bcb6caf8c9c9cfd19ed08af4
OsaidAlhomedy <o.alhomedy@gmail.com>
2023-03-19 20:43:36 +0300

    added analitcs

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,25 +1,35 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useContext } from "@builder.io/qwik";
 import { Certificates } from "../Certificates";
 import { Experience } from "../Experiance";
 import { Friends } from "../Friends";
 import { Knowledge } from "../Knowledge";
+import { Context } from "../../root";
 
 export const Main = component$(() => {
+  const context = useContext(Context);
+  const changeLanguage = $((lang: "en" | "ar") => {
+    console.log(lang);
+    context.lang = lang;
+    context.dir = lang === "ar" ? "rtl" : "ltr";
+  });
   return (
     <div class="flex flex-col gap-6 w-full">
       <div class="sticky top-0 left-0 z-10 flex h-[48.7px] justify-end overflow-x-auto border-b border-details bg-auto px-2 transition-all md:px-4">
         <div class="flex items-center gap-3 md:gap-2">
-          {/* <div class="flex items-center gap-3 md:gap-2">
-            <button class="rounded-md border border-details bg-misc font-medium shadow-sm transition-all hover:bg-details text-xs py-1 px-2">
+          <div class="flex items-center gap-3 md:gap-2">
+            <button
+              onClick$={() => changeLanguage("en")}
+              class="rounded-md border border-details bg-misc font-medium shadow-sm transition-all hover:bg-details text-xs py-1 px-2"
+            >
               EN
             </button>
             <button
-              disabled
+              onClick$={() => changeLanguage("ar")}
               class="rounded-md border border-details bg-misc font-medium shadow-sm transition-all hover:bg-details text-xs py-1 px-2 !bg-main text-misc-font hover:bg-main"
             >
               AR
             </button>
-          </div> */}
+          </div>
         </div>
       </div>
       <div class="flex flex-col gap-9 px-4 pb-32">
--- src/root.tsx
+++ src/root.tsx
@@ -1,14 +1,39 @@
-import { component$ } from "@builder.io/qwik";
+import {
+  component$,
+  createContextId,
+  useContextProvider,
+  useStore,
+} from "@builder.io/qwik";
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
+import { inject } from '@vercel/analytics';
 
 import "./global.css";
 
+export type CTX = {
+  theme: "light" | "dark";
+  lang: "en" | "ar";
+  dir: "ltr" | "rtl";
+};
+
+export const Context = createContextId<CTX>("MainContext");
+
+inject();
+
 export default component$(() => {
+  const store = useStore<CTX>({
+    theme: "light",
+    lang: "en",
+    dir: "ltr",
+  });
+
+  useContextProvider(Context, store);
+
+
   /**
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
    * immediately followed by the document's <head> and <body>.
@@ -23,7 +48,7 @@ export default component$(() => {
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
       </head>
-      <body lang="en" class="overflow-x-hidden">
+      <body lang={store.lang} dir={store.dir} class="overflow-x-hidden">
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
