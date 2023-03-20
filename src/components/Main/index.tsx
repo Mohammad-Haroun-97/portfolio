8edd0cd561ca8ecb65dc5a5bc3c97925230016fd
Osaid Alhomedy <o.alhomedy@gmail.com>
2023-03-20 11:39:37 +0300

    added something

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -7,7 +7,7 @@ import { Knowledge } from "../Knowledge";
 export const Main = component$(() => {
   return (
     <div class="flex flex-col gap-6 w-full">
-      <div class="sticky flex justify-center md:justify-end top-0 left-0 z-10 h-[60px] overflow-x-auto border-b border-spacing-3 border-details bg-auto px-2 transition-all md:px-4">
+      <div class="sticky flex justify-center md:justify-end top-0 left-0 z-10 h-[48.7px] overflow-x-auto border-b border-spacing-3 border-details bg-auto px-2 transition-all md:px-4">
         <span id="cat" class="absolute mr-10" />
       </div>
       <div class="flex flex-col gap-9 px-4 pb-32">
